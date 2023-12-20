4ee633f6c2e407908b31935862a51aa91339fc57
Jorja Elliott <elliottjorja@gmail.com>
2023-12-20 14:22:53 -0600

    update to parsing script

--- scripts/parsing.results.R
+++ scripts/parsing.results.R
@@ -1,44 +1,63 @@
 res <- read.csv("../results/sim.results.csv")
 
 # Parsing out Aa and Aa
-sum.res <- as.data.frame(matrix(0,6,6))
-colnames(sum.res) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
-row.names(sum.res) <- c("Aa1 / Aa2", "Aa2 / Aa1", "Aa1 * Aa2", "Aa + Aa", 
+sum.resAa.Aa <- as.data.frame(matrix(0,6,6))
+colnames(sum.resAa.Aa) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
+row.names(sum.resAa.Aa) <- c("Aa1 / Aa2", "Aa2 / Aa1", "Aa1 * Aa2", "Aa1 + Aa2", 
                         "Aa1 - Aa2", "Aa2 - Aa1")
 
 for(i in seq(from=1, by=8, length.out=1000)){
   for(j in 1:5){
-    sum.res[, j] <- sum.res[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
+    sum.resAa.Aa[, j] <- sum.resAa.Aa[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
   }
   counter <- 1
   for(k in (i + 2):(i + 7)){
     if(sum(is.na(res[k, 3:7])) == 5){
-      sum.res[counter, 6] <- sum.res[counter, 6] + 1
+      sum.resAa.Aa[counter, 6] <- sum.resAa.Aa[counter, 6] + 1
     }
     counter <- counter + 1
   }
 }
-AaAa <- sum.res
+
 
 # Parsing out Aa and Ad
-sum.res <- as.data.frame(matrix(0,6,6))
-colnames(sum.res) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
-row.names(sum.res) <- c("Aa1 / Aa2", "Aa2 / Aa1", "Aa1 * Aa2", "Aa + Aa", 
-                        "Aa1 - Aa2", "Aa2 - Aa1")
+sum.resAa.Ad <- as.data.frame(matrix(0,6,6))
+colnames(sum.resAa.Ad) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
+row.names(sum.resAa.Ad) <- c("Aa / Ad", "Ad / Aa", "Aa * Ad", "Aa + Ad", 
+                        "Aa - Ad", "Ad - Aa") # these now names change
 
-for(i in seq(from=1, by=8, length.out=1000)){ # this line changes
+for(i in seq(from=8001, by=8, length.out=1000)){ # this line changes
   for(j in 1:5){
-    sum.res[, j] <- sum.res[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
+    sum.resAa.Ad[, j] <- sum.resAa.Ad[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
   }
   counter <- 1
   for(k in (i + 2):(i + 7)){
     if(sum(is.na(res[k, 3:7])) == 5){
-      sum.res[counter, 6] <- sum.res[counter, 6] + 1
+      sum.resAa.Ad[counter, 6] <- sum.resAa.Ad[counter, 6] + 1
     }
     counter <- counter + 1
   }
 }
 
+
+# Parsing out Ad and Ad
+sum.resAd.Ad <- as.data.frame(matrix(0,6,6))
+colnames(sum.resAd.Ad) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
+row.names(sum.resAd.Ad) <- c("Ad1 / Ad2", "Ad2 / Ad1", "Ad1 * Ad2", "Ad1 + Ad2", 
+                             "Ad1 - Ad2", "Ad2 - Ad1")
+
+for(i in seq(from=24001, by=8, length.out=1000)){ # this line changes
+  for(j in 1:5){
+    sum.resAd.Ad[, j] <- sum.resAd.Ad[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
+  }
+  counter <- 1
+  for(k in (i + 2):(i + 7)){
+    if(sum(is.na(res[k, 3:7])) == 5){
+      sum.resAd.Ad[counter, 6] <- sum.resAd.Ad[counter, 6] + 1
+    }
+    counter <- counter + 1
+  }
+}
 
 
 
