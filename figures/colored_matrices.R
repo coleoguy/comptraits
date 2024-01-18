b708d3f667005d4f25a742507929a1750396e19f
Jorja Elliott <elliottjorja@gmail.com>
2024-01-18 10:58:54 -0600

    create figs, update fig script

--- figures/colored_matrices.R
+++ figures/colored_matrices.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(viridis)
 
 ##### colored matrix - Aa & Aa #####
-data <- read.csv("../results/resAa.Aa.csv", header = TRUE, row.names = 1)
+data <- read.csv("../results/a.a.tab.csv", header = TRUE, row.names = 1)
 
 # Convert the data frame to a matrix
 data_matrix <- as.matrix(data)
@@ -12,20 +12,23 @@ data_long <- expand.grid(Row = rownames(data_matrix),
                          Column = colnames(data_matrix))
 data_long$Value <- as.vector(data_matrix)
 
+# Open PDF device
+pdf("../figures/a.a.pdf")
+
 # Creating the plot
 ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
   geom_tile() +
-  scale_fill_viridis(name = "Value", limits = c(0, 1000)) +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
   labs(x = "Architectures", y = "Functions", title = "Component Traits: Aa & Aa") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
 ##### colored matrix - Aa & Aa #####
 
-
-
-
 ##### colored matrix - Aa & Ad #####
-data <- read.csv("../results/resAa.Ad.csv", header = TRUE, row.names = 1)
+data <- read.csv("../results/a.d.tab.csv", header = TRUE, row.names = 1)
 
 # Convert the data frame to a matrix
 data_matrix <- as.matrix(data)
@@ -35,20 +38,23 @@ data_long <- expand.grid(Row = rownames(data_matrix),
                          Column = colnames(data_matrix))
 data_long$Value <- as.vector(data_matrix)
 
+# Open PDF device
+pdf("../figures/a.d.pdf")
+
 # Creating the plot
 ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
   geom_tile() +
-  scale_fill_viridis(name = "Value", limits = c(0, 1000)) +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
   labs(x = "Architectures", y = "Functions", title = "Component Traits: Aa & Ad") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
 ##### colored matrix - Aa & Ad #####
 
-
-
-
 ##### colored matrix - Ad & Ad #####
-data <- read.csv("../results/resAd.Ad.csv", header = TRUE, row.names = 1)
+data <- read.csv("../results/d.d.tab.csv", header = TRUE, row.names = 1)
 
 # Convert the data frame to a matrix
 data_matrix <- as.matrix(data)
@@ -58,14 +64,425 @@ data_long <- expand.grid(Row = rownames(data_matrix),
                          Column = colnames(data_matrix))
 data_long$Value <- as.vector(data_matrix)
 
+# Open PDF device
+pdf("../figures/d.d.pdf")
+
 # Creating the plot
 ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
   geom_tile() +
-  scale_fill_viridis(name = "Value", limits = c(0, 1000)) +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
   labs(x = "Architectures", y = "Functions", title = "Component Traits: Ad & Ad") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
 ##### colored matrix - Ad & Ad #####
+
+### ### ### ### ### ### ### ### ### ###
+
+##### colored matrix - Aa & AaAa #####
+data <- read.csv("../results/a.AaAa.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/a.AaAa.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: Aa & AaAa") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - Aa & AaAa #####
+
+##### colored matrix - Aa & AaAd #####
+data <- read.csv("../results/a.AaAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/a.AaAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: Aa & AaAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - Aa & AaAd #####
+
+##### colored matrix - Aa & AdAd #####
+data <- read.csv("../results/a.AdAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/a.AdAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: Aa & AdAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - Aa & AdAd #####
+
+### ### ### ### ### ### ### ### ### ###
+
+##### colored matrix - Ad & AaAa #####
+data <- read.csv("../results/d.AaAa.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/d.AaAa.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: Ad & AaAa") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - Ad & AaAa #####
+
+##### colored matrix - Ad & AaAd #####
+data <- read.csv("../results/d.AaAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/d.AaAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: Ad & AaAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - Ad & AaAd #####
+
+##### colored matrix - Ad & AdAd #####
+data <- read.csv("../results/d.AdAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/d.AdAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: Ad & AdAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - Ad & AdAd #####
+
+### ### ### ### ### ### ### ### ### ###
+
+##### colored matrix - AaAa & AaAa #####
+data <- read.csv("../results/AaAa.AaAa.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AaAa.AaAa.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AaAa & AaAa") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AaAa & AaAa #####
+
+##### colored matrix - AaAa & AaAd #####
+data <- read.csv("../results/AaAa.AaAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AaAa.AaAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AaAa & AaAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AaAa & AaAd #####
+
+##### colored matrix - AaAa & AdAd #####
+data <- read.csv("../results/AaAa.AdAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AaAa.AdAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AaAa & AdAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AaAa & AdAd #####
+
+### ### ### ### ### ### ### ### ### ###
+
+##### colored matrix - AaAd & AaAa #####
+data <- read.csv("../results/AaAd.AaAa.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AaAd.AaAa.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AaAd & AaAa") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AaAd & AaAa #####
+
+##### colored matrix - AaAd & AaAd #####
+data <- read.csv("../results/AaAd.AaAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AaAd.AaAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AaAd & AaAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AaAd & AaAd #####
+
+##### colored matrix - AaAd & AdAd #####
+data <- read.csv("../results/AaAd.AdAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AaAd.AdAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AaAd & AdAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AaAd & AdAd #####
+
+### ### ### ### ### ### ### ### ### ###
+
+##### colored matrix - AdAd & AaAa #####
+data <- read.csv("../results/AdAd.AaAa.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AdAd.AaAa.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AdAd & AaAa") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AdAd & AaAa #####
+
+##### colored matrix - AdAd & AaAd #####
+data <- read.csv("../results/AdAd.AaAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AdAd.AaAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AdAd & AaAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AdAd & AaAd #####
+
+##### colored matrix - AdAd & AdAd #####
+data <- read.csv("../results/AdAd.AdAd.tab.csv", header = TRUE, row.names = 1)
+
+# Convert the data frame to a matrix
+data_matrix <- as.matrix(data)
+
+# Creating a data frame suitable for ggplot
+data_long <- expand.grid(Row = rownames(data_matrix), 
+                         Column = colnames(data_matrix))
+data_long$Value <- as.vector(data_matrix)
+
+# Open PDF device
+pdf("../figures/AdAd.AdAd.pdf")
+
+# Creating the plot
+ggplot(data_long, aes(x = Column, y = Row, fill = Value)) +
+  geom_tile() +
+  scale_fill_viridis(name = "Value", limits = c(0, 1)) +
+  labs(x = "Architectures", y = "Functions", title = "Component Traits: AdAd & AdAd") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#close the device
+dev.off()
+##### colored matrix - AdAd & AdAd #####
+
+
+
+
+
 
 
 
--- scripts/parsing.results.R
+++ scripts/parsing.results.R
@@ -1,73 +1,6 @@
 res <- read.csv("../results/sim.results.csv")
-
-# Parsing out Aa and Aa
-sum.resAa.Aa <- as.data.frame(matrix(0,6,6))
-colnames(sum.resAa.Aa) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
-row.names(sum.resAa.Aa) <- c("Aa1 / Aa2", "Aa2 / Aa1", "Aa1 * Aa2", "Aa1 + Aa2", 
-                        "Aa1 - Aa2", "Aa2 - Aa1")
-
-for(i in seq(from=1, by=8, length.out=1000)){
-  for(j in 1:5){
-    sum.resAa.Aa[, j] <- sum.resAa.Aa[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
-  }
-  counter <- 1
-  for(k in (i + 2):(i + 7)){
-    if(sum(is.na(res[k, 3:7])) == 5){
-      sum.resAa.Aa[counter, 6] <- sum.resAa.Aa[counter, 6] + 1
-    }
-    counter <- counter + 1
-  }
-}
-write.csv(sum.resAa.Aa, file = "../results/resAa.Aa.csv", row.names=T)
-
-
-# Parsing out Aa and Ad
-sum.resAa.Ad <- as.data.frame(matrix(0,6,6))
-colnames(sum.resAa.Ad) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
-row.names(sum.resAa.Ad) <- c("Aa / Ad", "Ad / Aa", "Aa * Ad", "Aa + Ad", 
-                        "Aa - Ad", "Ad - Aa") # these now names change
-
-for(i in seq(from=8001, by=8, length.out=1000)){ # this line changes
-  for(j in 1:5){
-    sum.resAa.Ad[, j] <- sum.resAa.Ad[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
-  }
-  counter <- 1
-  for(k in (i + 2):(i + 7)){
-    if(sum(is.na(res[k, 3:7])) == 5){
-      sum.resAa.Ad[counter, 6] <- sum.resAa.Ad[counter, 6] + 1
-    }
-    counter <- counter + 1
-  }
-}
-write.csv(sum.resAa.Ad, file = "../results/resAa.Ad.csv", row.names=T)
-
-
-# Parsing out Ad and Ad
-sum.resAd.Ad <- as.data.frame(matrix(0,6,6))
-colnames(sum.resAd.Ad) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
-row.names(sum.resAd.Ad) <- c("Ad1 / Ad2", "Ad2 / Ad1", "Ad1 * Ad2", "Ad1 + Ad2", 
-                             "Ad1 - Ad2", "Ad2 - Ad1")
-
-for(i in seq(from=24001, by=8, length.out=1000)){ # this line changes
-  for(j in 1:5){
-    sum.resAd.Ad[, j] <- sum.resAd.Ad[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
-  }
-  counter <- 1
-  for(k in (i + 2):(i + 7)){
-    if(sum(is.na(res[k, 3:7])) == 5){
-      sum.resAd.Ad[counter, 6] <- sum.resAd.Ad[counter, 6] + 1
-    }
-    counter <- counter + 1
-  }
-}
-write.csv(sum.resAd.Ad, file = "../results/resAd.Ad.csv", row.names=T)
-
-
-# Parsing out epistatic architectures
-
-res <- read.csv("../results/sim.results-epistatic.csv")
-elar1 <- "Aa"
-elar2 <- "Aa"
+#elar1 <- "Aa"
+#elar2 <- "Aa"
 
 GetTable <- function(res, elar1, elar2){
   elar1hits <- which(res$trait == elar1)
@@ -75,8 +8,8 @@ GetTable <- function(res, elar1, elar2){
   starts <- intersect(elar1hits, elar2hits)
   sum.res <- as.data.frame(matrix(0,6,6))
   colnames(sum.res) <- c("Aa","Ad","AaAa","AaAd","AdAd", "NA")
-  row.names(sum.res) <- c("1 / 2", "2 / 1", "1 * 2", "1 + 2", 
-                          "1 - 2", "2 - 1")
+  row.names(sum.res) <- c("1. / 2.", "2. / 1.", "1. * 2.", "1. + 2.", 
+                          "1. - 2.", "2. - 1.")
   for(i in starts){ 
     for(j in 1:5){
       sum.res[, j] <- sum.res[, j] + as.numeric(!is.na(res[(i + 2):(i + 7), (j + 2)]))
@@ -94,7 +27,74 @@ GetTable <- function(res, elar1, elar2){
   return(sum.res)
 }
 
-a.a.res <- GetTable(res, elar1, elar2)
-d.e.res <- GetTable(res, elar1="Ad", elar2="AaAa")
-e.e.res1 <- GetTable(res, elar1="AdAd", elar2="AaAa")
-write.csv(sum.resAd.Ad, file = "../results/resAd.Ad.csv", row.names=T)
+#component traits: Aa & Aa
+a.a.res <- GetTable(res, elar1="Aa", elar2="Aa")
+write.csv(a.a.res, file="../results/a.a.tab.csv")
+
+#component traits: Ad & Ad
+d.d.res <- GetTable(res, elar1="Ad", elar2="Ad")
+write.csv(d.d.res, file="../results/d.d.tab.csv")
+
+#component traits: Aa & Ad
+a.d.res <- GetTable(res, elar1="Aa", elar2="Ad")
+write.csv(a.d.res, file="../results/a.d.tab.csv")
+
+#component traits: Aa & AaAa
+a.AaAa.res <- GetTable(res, elar1="Aa", elar2="AaAa")
+write.csv(a.AaAa.res, file="../results/a.AaAa.tab.csv")
+#component traits: Aa & AaAd
+a.AaAd.res <- GetTable(res, elar1="Aa", elar2="AaAd")
+write.csv(a.AaAd.res, file="../results/a.AaAd.tab.csv")
+#component traits: Aa & AdAd
+a.AdAd.res <- GetTable(res, elar1="Aa", elar2="AdAd")
+write.csv(a.AdAd.res, file="../results/a.AdAd.tab.csv")
+
+#component traits: Ad & AaAa
+d.AaAa.res <- GetTable(res, elar1="Ad", elar2="AaAa")
+write.csv(d.AaAa.res, file="../results/d.AaAa.tab.csv")
+#component traits: Ad & AaAd
+d.AaAd.res <- GetTable(res, elar1="Ad", elar2="AaAd")
+write.csv(d.AaAd.res, file="../results/d.AaAd.tab.csv")
+#component traits: Ad & AdAd
+d.AdAd.res <- GetTable(res, elar1="Ad", elar2="AdAd")
+write.csv(d.AdAd.res, file="../results/d.AdAd.tab.csv")
+
+#component traits: AaAa & AaAa
+AaAa.AaAa.res <- GetTable(res, elar1="AaAa", elar2="AaAa")
+write.csv(AaAa.AaAa.res, file="../results/AaAa.AaAa.tab.csv")
+#component traits: AaAa & AaAd
+AaAa.AaAd.res <- GetTable(res, elar1="AaAa", elar2="AaAd")
+write.csv(AaAa.AaAd.res, file="../results/AaAa.AaAd.tab.csv")
+#component traits: AaAa & AdAd
+AaAa.AdAd.res <- GetTable(res, elar1="AaAa", elar2="AdAd")
+write.csv(AaAa.AdAd.res, file="../results/AaAa.AdAd.tab.csv")
+
+#component traits: AaAd & AaAa
+AaAd.AaAa.res <- GetTable(res, elar1="AaAd", elar2="AaAa")
+write.csv(AaAd.AaAa.res, file="../results/AaAd.AaAa.tab.csv")
+#component traits: AaAd & AaAd
+AaAd.AaAd.res <- GetTable(res, elar1="AaAd", elar2="AaAd")
+write.csv(AaAd.AaAd.res, file="../results/AaAd.AaAd.tab.csv")
+#component traits: AaAd & AdAd
+AaAd.AdAd.res <- GetTable(res, elar1="AaAd", elar2="AdAd")
+write.csv(AaAd.AdAd.res, file="../results/AaAd.AdAd.tab.csv")
+
+#component traits: AdAd & AaAa
+AdAd.AaAa.res <- GetTable(res, elar1="AdAd", elar2="AaAa")
+write.csv(AdAd.AaAa.res, file="../results/AdAd.AaAa.tab.csv")
+#component traits: AdAd & AaAd
+AdAd.AaAd.res <- GetTable(res, elar1="AdAd", elar2="AaAd")
+write.csv(AdAd.AaAd.res, file="../results/AdAd.AaAd.tab.csv")
+#component traits: AdAd & AdAd
+AdAd.AdAd.res <- GetTable(res, elar1="AdAd", elar2="AdAd")
+write.csv(AdAd.AdAd.res, file="../results/AdAd.AdAd.tab.csv")
+
+
+
+
+
+
+
+
+
+
