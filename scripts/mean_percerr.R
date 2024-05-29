d1f9a012a33ecc6b3221fd16ca54d7e314b28680
Jorja Elliott <elliottjorja@gmail.com>
2024-05-29 16:40:58 -0500

    Update mean_percerr.R

--- scripts/mean_percerr.R
+++ scripts/mean_percerr.R
@@ -4,29 +4,47 @@
 # the percent error results file, which only contains results for when both 
 # elemental traits are additive
 
-library(dplyr)
 
 # read in results file with calculated beta coeff for +, -, and *, and percent errors
 data <- read.csv("../results/res_a_percerr.csv")
 
+
+
+# ELEMENTAL
+# Filter the data for "Aa + Aa" in the "trait" column
+ell_pe <- data[data$trait == "Aa", ]
+
+# Calculate the mean of the "PercentError" column, ignoring any NA values
+ell_pe <- mean(ell_pe$PercentError, na.rm = TRUE)
+
+
+
 # ADDITION
-# Filter the data for "Aa + Aa" in the "trait" column and calculate the mean of "PercentError"
-mean_pe_add <- data %>%
-  filter(trait == "Aa + Aa") %>%
-  summarise(mean_percent_error = mean(PercentError, na.rm = TRUE))
+# Filter the data for "Aa + Aa" in the "trait" column
+add_pe <- data[data$trait == "Aa + Aa", ]
+
+# Calculate the mean of the "PercentError" column, ignoring any NA values
+add_pe <- mean(add_pe$PercentError, na.rm = TRUE)
 
 
-# SUBTRACTION (both 1 - 2 and 2 - 1)
-# Filter the data for "Aa - Aa" in the "trait" column and calculate the mean of "PercentError"
-mean_pe_sub <- data %>%
-  filter(trait == "Aa - Aa") %>%
-  summarise(mean_percent_error = mean(PercentError, na.rm = TRUE))
+
+# SUBTRACTION
+# Filter the data for "Aa + Aa" in the "trait" columns
+sub_pe <- data[data$trait == "Aa - Aa", ]
+
+# Calculate the mean of the "PercentError" column, ignoring any NA values
+sub_pe <- mean(sub_pe$PercentError, na.rm = TRUE)
+
+
 
 
 # MULTIPLICATION
-# Filter the data for "Aa * Aa" in the "trait" column and calculate the mean of "PercentError"
-mean_pe_mult <- data %>%
-  filter(trait == "Aa * Aa") %>%
-  summarise(mean_percent_error = mean(PercentError, na.rm = TRUE))
+# Filter the data for "Aa * Aa" in the "trait" column
+mult_pe <- data[data$trait == "Aa * Aa", ]
+
+# Calculate the mean of the "PercentError" column, ignoring any NA values
+mult_pe <- mean(mult_pe$PercentError, na.rm = TRUE)
+
+
 
 
