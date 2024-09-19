82441d21bcb158342000a644e82d01373ba57baa
Jorja Elliott <elliottjorja@gmail.com>
2024-09-19 13:51:51 -0500

    reran ft sims with updated sigmas, generated new selection figs, added points to parameter figs

--- figures/supp/parameters.R
+++ figures/supp/parameters.R
@@ -1,3 +1,9 @@
+# Jorja Burch - jorjaelliott@tamu.edu
+
+# this script generates the supp plot (3 panel) which shows the range in phenotypes
+# for the inferred and generating architectures, and the distribution of fitnesses,
+# and the optimum phenotype. For each inferred arch (AaAa, AaAd, AdAd)
+
 dat <- read.csv("../../results/selection simulation results/parameters.csv")
 
 getW <- function(low, high, opt, sigma){
@@ -11,6 +17,11 @@ getW <- function(low, high, opt, sigma){
 
 xlims <- c(min(dat[1,1:4]), max(dat[1,1:4]))
 ylims <- c(-.05,1)
+
+# plot 1 - AaAa
+#open plod as pdf device
+pdf("AaAa_parameters.pdf")
+
 plot(x=0,y=0,xlim=xlims,ylim=ylims,col="white",
      xlab="phenotype", ylab="fitness")
 lines(x=c(dat[1,1],dat[1,2]),y=c(-0.01,-0.01), lwd=3,col="blue")
@@ -30,3 +41,74 @@ lines(x=fitdatgen[[2]],
 lines(x=fitdatinf[[2]],
       y=fitdatinf[[1]],
       col="red")
+# add point to each line for phenotype at gen 1
+points(8.84805, -0.01, col="blue", pch=19)
+points(8.480567, -0.04, col="red", pch=19)
+
+# close the device
+dev.off()
+
+
+# plot 2 - AaAd
+#open plod as pdf device
+pdf("AaAd_parameters.pdf")
+
+plot(x=0,y=0,xlim=xlims,ylim=ylims,col="white",
+     xlab="phenotype", ylab="fitness")
+lines(x=c(dat[2,1],dat[2,2]),y=c(-0.01,-0.01), lwd=3,col="blue")
+lines(x=c(dat[2,3],dat[2,4]),y=c(-0.04,-0.04), lwd=3,col="red")
+abline(v=dat[2,5],lty=2)
+fitdatgen <- getW(dat$low.gen[2],
+                  dat$high.gen[2], 
+                  dat$opt[2],
+                  dat$sigma1[2])
+fitdatinf <- getW(dat$low.inf[2],
+                  dat$high.inf[2], 
+                  dat$opt[2],
+                  dat$sigma2[2])
+lines(x=fitdatgen[[2]],
+      y=fitdatgen[[1]],
+      col="blue")
+lines(x=fitdatinf[[2]],
+      y=fitdatinf[[1]],
+      col="red")
+
+# add point to each line for phenotype at gen 1
+points(8.883328, -0.01, col="blue", pch=19)
+points(9.440011, -0.04, col="red", pch=19)
+
+# close the device
+dev.off()
+
+
+# plot 3 - AdAd
+
+#open plod as pdf device
+pdf("AdAd_parameters.pdf")
+
+plot(x=0,y=0,xlim=xlims,ylim=ylims,col="white",
+     xlab="phenotype", ylab="fitness")
+lines(x=c(dat[3,1],dat[3,2]),y=c(-0.01,-0.01), lwd=3,col="blue")
+lines(x=c(dat[3,3],dat[3,4]),y=c(-0.04,-0.04), lwd=3,col="red")
+abline(v=dat[3,5],lty=2)
+fitdatgen <- getW(dat$low.gen[3],
+                  dat$high.gen[3], 
+                  dat$opt[3],
+                  dat$sigma1[3])
+fitdatinf <- getW(dat$low.inf[3],
+                  dat$high.inf[3], 
+                  dat$opt[3],
+                  dat$sigma2[3])
+lines(x=fitdatgen[[2]],
+      y=fitdatgen[[1]],
+      col="blue")
+lines(x=fitdatinf[[2]],
+      y=fitdatinf[[1]],
+      col="red")
+
+# add point to each line for phenotype at gen 1
+points(7.312431, -0.01, col="blue", pch=19)
+points(7.385211, -0.04, col="red", pch=19)
+
+# close the device
+dev.off()
--- scripts/selection simulation/LCA-ft.sim.R
+++ scripts/selection simulation/LCA-ft.sim.R
@@ -40,22 +40,22 @@ res.aa.compound.trait <- SimulateCond(reps = reps, gen = gen, loci = loci,
                                       mu1 = 30, mu2 = 3,
                                       beta1 =7.576, beta2 = 0.788,
                                       single.arch = NULL)
-#write.csv(res.aa.compound.trait, 
-          #file = "../../results/selection simulation results/res.aa.compound.trait.csv")
+write.csv(res.aa.compound.trait, 
+          file = "../../results/selection simulation results/res.aa.compound.trait.csv")
 res.aa.single.trait <- SimulateCond(reps = reps, gen = gen, loci = loci,
                                     N = N, arch1 = NULL,
                                     arch2 = NULL, deffnc = "none",
                                     sigma = 0.38, opt = 9.6, mu1 = 7.772,
                                     beta1 = 1.891, single.arch = "add.x.add")
-#write.csv(res.aa.single.trait, 
-          #file = "../../results/selection simulation results/res.aa.single.trait.csv")
+write.csv(res.aa.single.trait, 
+          file = "../../results/selection simulation results/res.aa.single.trait.csv")
 res.aa.single.trait100 <- SimulateCond(reps = reps, gen = gen, loci = loci,
                                     N = 100, arch1 = NULL,
                                     arch2 = NULL, deffnc = "none",
                                     sigma = 0.38, opt = 9.6, mu1 = 7.772,
                                     beta1 = 1.891, single.arch = "add.x.add")
-#write.csv(res.aa.single.trait100, 
-          #file = "../../results/selection simulation results/res.aa.single.trait100.csv")
+write.csv(res.aa.single.trait100, 
+          file = "../../results/selection simulation results/res.aa.single.trait100.csv")
 
 # plot generating trait arch
 plot(res.aa.compound.trait[1,], type="l", 
@@ -112,15 +112,15 @@ res.ad.compound.trait <- SimulateCond(reps = reps, gen = gen, loci = loci,
                                       mu1 = 30, mu2 = 3,
                                       beta1 =7.52, beta2 = 0.76,
                                       single.arch = NULL)
-#write.csv(res.ad.compound.trait, 
-          #file = "../../results/selection simulation results/res.ad.compound.trait.csv")
+write.csv(res.ad.compound.trait, 
+          file = "../../results/selection simulation results/res.ad.compound.trait.csv")
 res.ad.single.trait <- SimulateCond(reps = reps, gen = gen, loci = loci,
                                     N = N, arch1 = NULL,
                                     arch2 = NULL, deffnc = "none",
                                     sigma = 0.34, opt = 10.231, mu1 = 9.44,
                                     beta1 = 3.165, single.arch = "add.x.dom")
-#write.csv(res.ad.single.trait, 
-          #file = "../../results/selection simulation results/res.ad.single.trait.csv")
+write.csv(res.ad.single.trait, 
+          file = "../../results/selection simulation results/res.ad.single.trait.csv")
 
 # plot generating trait arch
 plot(res.ad.compound.trait[1,], type="l", 
@@ -171,15 +171,15 @@ res.dd.compound.trait <- SimulateCond(reps = reps, gen = gen, loci = loci,
                                       mu1 = 50, mu2 = 6,
                                       beta1 = 9.39, beta2 = 1.694,
                                       single.arch = NULL)
-#write.csv(res.dd.compound.trait, 
-          #file = "../../results/selection simulation results/res.dd.compound.trait.csv")
+write.csv(res.dd.compound.trait, 
+        file = "../../results/selection simulation results/res.dd.compound.trait.csv")
 res.dd.single.trait <- SimulateCond(reps = reps, gen = gen, loci = loci,
                                     N = N, arch1 = NULL,
                                     arch2 = NULL, deffnc = "none",
                                     sigma = 0.38, opt = 9.038, mu1 = 7.275,
                                     beta1 = 1.763, single.arch = "dom.x.dom")
-#write.csv(res.dd.single.trait, 
-          #file = "../../results/selection simulation results/res.dd.single.trait.csv")
+write.csv(res.dd.single.trait, 
+          file = "../../results/selection simulation results/res.dd.single.trait.csv")
 
 # plot generating trait arch
 plot(res.dd.compound.trait[1,], type="l", 
